136eefd5477f4dffd42fa277dd0b78bfc2c56ed7
GabriMac <gabri1998gabri@gmail.com>
2023-04-22 00:02:02 +0200

    added index

---
+++ 0-intro/index.js
@@ -0,0 +1,12 @@
+//  <-- This is a comment, it will not be executed
+
+
+
+// console.log is a function that prints to the console
+// console.log('Hello World!')
+
+// You can also print numbers
+// console.log(7)
+
+// And also arithmetic expressions
+// console.log(7+3)
