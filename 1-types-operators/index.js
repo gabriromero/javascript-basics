5a49fc6bbc6fdd552f33e169ab32ec55f9f8e70a
GabriMac <gabri1998gabri@gmail.com>
2023-04-23 09:06:27 +0200

    added index part 1

---
+++ 1-types-operators/index.js
@@ -0,0 +1,18 @@
+// Problema 1. Printar un Number, un String y un Boolean
+// Utiliza el typeof para comprobarlo
+// console.log();
+// console.log();
+// console.log();
+
+// Problema 2. Guarda en una variable let un String y luego printalo.
+// console.log();
+
+// Problema 3. Guarda en una constante un Number y fuerza 
+//             a que de error sobreescribiéndolo
+// ???
+// console.log();
+
+// Problema 4. Añade algo en cada ??? para que acabe dando true
+// console.log( 1 == ???);
+// console.log( true != ???);
+// console.log( (true || false) && (true && ???) );
