19031911b7e1b428b66ea6e584f0d67519568c43
GabriMac <gabri1998gabri@gmail.com>
2023-04-26 22:48:30 +0200

    finalizado index.js parte 2

---
+++ 2-conditionals/index.js
@@ -0,0 +1,18 @@
+// Problema 1. A partir del numero de la pokedex, printa a que generación pertenece suponiendo que solo hay 4 regiones.
+//
+// let numeroPokedex = 33
+
+// Problema 2. Genera un número aleatorio entre 1 y 10 y printa ganador si el número es 7
+//
+
+// Problema 3. A partir de una variable nombre, printa si es o no un nombre correcto
+// Un nombre correcto es todo aquel nombre que su primera letra empieza por mayúscula 
+//
+// let nombre = "alejandro"
+
+// Problema 4. A partir de una variable numero, printa si es o no un numero estrella
+// Un numero estrella es todo aquel que sea divisible entre 3 y que su último dígito acabe en 2
+//
+// let numero = 1
+// 
+// 
